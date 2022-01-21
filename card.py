68c6a16c0d66e445e5b46d0362f2705e5bfcc7e3
Alan MONTOYA <alanmontoya@felipemotta.com>
2022-01-21 06:02:59 -0500

    Card Class implementation

--- card.py
+++ card.py
@@ -1,2 +1,11 @@
+import random
+
 class Card:
-    pass
+    """ This will be able to track the points and the next card
+    """
+
+    def __init__(self) -> None:
+        self.value = 0
+
+    def roll_card(self):
+        self.value = random.randint(1, 13)
